d881dd95f63ca607952b66f4c2f65c4e2d9743c6
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-02 17:03:29 +0800

    feat: score styles

--- src/game/status/score.tsx
+++ src/game/status/score.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Score: React.FC = () => <div className={classNames('status-score')} />;
+const Score: React.FC = () => (
+  <div className={classNames('status-score')}>
+    <div className={classNames('status-score-title')}>score</div>
+    <div className={classNames('status-score-value-container')}>
+      <div className={classNames('status-score-value')}>152</div>
+    </div>
+  </div>
+);
 
 export default Score;
