d5606b4948c32c73f25ce3f9dc9ac8d0dfb271e0
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-02 17:23:11 +0800

    feat: array of blocks

--- src/game/status/hold.tsx
+++ src/game/status/hold.tsx
@@ -1,6 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import classNames from 'classnames';
 
-const Hold: React.FC = () => <div className={classNames('status-hold')} />;
+const Hold: React.FC = () => {
+  const blksArr = [
+    [1, 1, 0],
+    [0, 1, 1],
+  ];
+
+  // show block
+  const showBlks = blksArr.map((row, index) => (
+    <div className={classNames('status-hold-blocks-row')}>
+      {row.map((i) => (i ? <div>1</div> : <div>0</div>))}
+    </div>
+  ));
+
+  return (
+    <div className={classNames('status-hold')}>
+      <div className={classNames('status-hold-title')}>HOLD</div>
+      <div className={classNames('status-hold-blocks-container')}>
+        {showBlks}
+      </div>
+    </div>
+  );
+};
 
 export default Hold;
--- src/game/status/score.tsx
+++ src/game/status/score.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 const Score: React.FC = () => (
   <div className={classNames('status-score')}>
-    <div className={classNames('status-score-title')}>score</div>
+    <div className={classNames('status-score-title')}>SCORE</div>
     <div className={classNames('status-score-value-container')}>
       <div className={classNames('status-score-value')}>152</div>
     </div>
