a5fed9988de17dfd73f0c6070eb55d3d314c1a9c
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-03 16:15:56 +0800

    feat: blocks layout

--- src/game/screen/index.tsx
+++ src/game/screen/index.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Screen: React.FC = () => {
-  const showBlks = [...Array(10)].map(() => {
-    return <div className={classNames('screen-block')} />;
+  const showBlks = [...Array(16)].map(() => {
+    return (
+      <div className={classNames('screen-row')}>
+        {[...Array(10)].map(() => {
+          return <div className={classNames('screen-block')} />;
+        })}
+      </div>
+    );
   });
 
   return (
     <div className={classNames('screen-container')}>
-      <div className={classNames('screen-row')}>{showBlks}</div>
+      <div className={classNames('screen-blocks-container')}>{showBlks}</div>
     </div>
   );
 };
