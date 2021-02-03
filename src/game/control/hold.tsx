05dcd21c01bb1307c9636101076e45ab12f64890
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-03 10:18:09 +0800

    feat: hold button styles

--- src/game/control/hold.tsx
+++ src/game/control/hold.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Hold: React.FC = () => (
-  <div className={classNames('control-hold-container')}>Hold</div>
+  <div className={classNames('control-hold-container')}>
+    <button type="button" className={classNames('control-hold-button')}>
+      H O L D
+    </button>
+  </div>
 );
 
 export default Hold;
