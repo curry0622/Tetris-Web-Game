43903d776495d858b67208b0c662b380ae466fbd
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-03 10:36:56 +0800

    feat: styles minor fix

--- src/game/control/drop.tsx
+++ src/game/control/drop.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Drop: React.FC = () => (
-  <div className={classNames('control-drop-container')}>Drop</div>
+  <div className={classNames('control-drop-container')}>
+    <button type="button" className={classNames('control-drop-button')}>
+      D R O P
+    </button>
+  </div>
 );
 
 export default Drop;
