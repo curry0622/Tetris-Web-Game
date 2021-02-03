bd247b737f8ea5516226350ddad211203a76462f
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-03 15:46:17 +0800

    feat: blocks in screen

--- src/game/screen/index.tsx
+++ src/game/screen/index.tsx
@@ -1,8 +1,17 @@
+/* eslint-disable arrow-body-style */
 import React from 'react';
 import classNames from 'classnames';
 
-const Screen: React.FC = () => (
-  <div className={classNames('screen-container')} />
-);
+const Screen: React.FC = () => {
+  const showBlks = [...Array(10)].map(() => {
+    return <div className={classNames('screen-block')} />;
+  });
+
+  return (
+    <div className={classNames('screen-container')}>
+      <div className={classNames('screen-row')}>{showBlks}</div>
+    </div>
+  );
+};
 
 export default Screen;
