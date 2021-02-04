688e80e77492b2da235646d02b202453af75e196
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-04 16:29:31 +0800

    refactor: re-structure files

--- src/game/index.tsx
+++ src/game/index.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
-import Board from './board';
+import Status from './status';
+import Screen from './screen';
+import Control from './control';
 
-const Game: React.FC = () => (
-  <main className={classNames('game-container')}>
-    <Board />
-  </main>
-);
+const Game: React.FC = () => {
+  // blocks to display
+  const [blks, setBlks] = useState<number[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  // function to determine blocks
+  const handleBlksChange = () => {};
+
+  // trigger handleBlksChange each second
+  useEffect(() => {
+    setInterval(handleBlksChange, 1000);
+  }, []);
+
+  return (
+    <main className={classNames('game-container')}>
+      <div className={classNames('board-container')}>
+        <Status />
+        <Screen blks={blks} />
+        <Control />
+      </div>
+    </main>
+  );
+};
 
 export default Game;
